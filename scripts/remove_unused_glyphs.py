3b88a4ceabe3a9fae8e8b8fe03a8d35dfcdef438
Raincole Lai <raincolee@gmail.com>
2014-12-21 03:40:01 +0800

    support glob in remove_unused_glyphs

--- scripts/remove_unused_glyphs.py
+++ scripts/remove_unused_glyphs.py
@@ -3,24 +3,26 @@
 import argparse
 import sys
 import io
+import glob
 import fontforge
 
 parser = argparse.ArgumentParser(description='Remove unused glyphs from a font to reduce size')
 parser.add_argument('font', help='original font')
-parser.add_argument('-t', '--text', nargs='+', required=True, help='text files including the characters to keep')
+parser.add_argument('-t', '--text', nargs='+', required=True, help='text files including the characters to keep(support glob)')
 parser.add_argument('-o', '--output', required=True, help='name of the output file(reduced font)')
 
 args = parser.parse_args()
 
 character_set = set()
 
-for filename in args.text:
-  with io.open(filename, encoding='utf-8', mode='r') as f:
-    character_set = character_set.union(set(f.read()))
+for fileglob in args.text:
+  for filename in glob.glob(fileglob):
+    with io.open(filename, encoding='utf-8', mode='r') as f:
+      character_set = character_set.union(set(f.read()))
 
 print('Character Set: ')
 for char in character_set:
-  sys.stdout.write(char)
+  sys.stdout.write(char.encode('utf-8'))
 
 print('\nGenerating Font...')
 
